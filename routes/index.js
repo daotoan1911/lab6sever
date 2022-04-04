7816cb7d1a2e362ccb1557bb5bc8e94383256a34
DAOTOAN <toandmph17429@fpt.edu.vn>
2022-04-04 10:13:17 +0700

    ok

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+var multer = require('multer');
+const e = require("express");
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    var random = Math.random();
+    cb(null,random + Date.now() + file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage, limits: {fileSize: 5*1024 }}).single('avatar');
+
+var upload1 = multer({ storage: storage, limits: {fileSize: 5*1024 }}).array('avatar', 5);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/profile', function(req, res ) {
+  upload(reg,res, function (err){
+    if (err){
+      res.render('index', {
+        title: err.message
+      });
+    }else {
+      res.render('index',
+          { title: 'Uploads thành công' +'Ktra thư mục uploads' });
+    }
+  })
+});
+
+router.post('/profilearray', function(req, res ) {
+  res.render('index', { title: 'Uploads thành công' +'Ktra thư mục uploads' });
+});
+module.exports = router;
